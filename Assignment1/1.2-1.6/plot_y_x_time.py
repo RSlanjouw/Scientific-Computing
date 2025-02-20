e95b305fd7d17cd2c3b759bcee30352883de2dc7
RSlanjouw <ruben.lanjouw14@gmail.com>
2025-02-20 11:52:19 +0100

    FIX

--- Assignment1/1.2-1.6/plot_y_x_time.py
+++ Assignment1/1.2-1.6/plot_y_x_time.py
@@ -4,9 +4,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from modules import error_analytical as ea
+from modules.functions import TTD_Eq
 
 
 show_analytic = True
+
+def generate():
+    y_top= 1
+    y_bottom = 0
+    y_left = y_right = 0
+    n = 50
+    dx = 1/n
+    dt = (dx**2)/4
+
+    y = [] 
+    frames_to_save = [10, 100, 1000, 10000]
+    domain = np.zeros((n ,n))
+    domain[0, :] = 1
+    x = np.linspace(0, 1, n)
+    dt_ = []
+
+    #run the diffusion while saving our four cases 
+    for i in range(10001):
+        domain = TTD_Eq(domain, dt, dx, 1)
+        if i in frames_to_save:
+            y.append(domain[::-1, 0])
+            dt_.append(dt * i)
+
+    #save the resulting data
+    np.save("data/x_valuesTTD.npy", x)
+    np.save("data/y_valuesTTD.npy", y)
+    np.save("data/dt_valuesTTD.npy", dt_)
+    np.save("data/frames_to_saveTTD.npy", frames_to_save)
+
 # check if data files are there
 try:
     x = np.load("data/x_valuesTTD.npy")
@@ -14,9 +44,7 @@ try:
     dt_ = np.load("data/dt_valuesTTD.npy")
     frames_to_save = np.load("data/frames_to_saveTTD.npy")
 except:
-    # run the generate_y_x_time_data.py file
-    import os
-    os.system("python generate_y_x_time_data.py") 
+    generate()
     x = np.load("data/x_valuesTTD.npy")
     y = np.load("data/y_valuesTTD.npy")
     dt_ = np.load("data/dt_valuesTTD.npy")
